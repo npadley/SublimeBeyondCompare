7149b7a7b8292d3f94730b684fe3f4b5e52a1e7b
Nick Padley <nick@nickpadley.com>
2016-04-22 09:01:38 -0500

    Testing travis

--- BeyondCompare.py
+++ BeyondCompare.py
@@ -1,21 +1,26 @@
-import sublime, sublime_plugin, os, webbrowser, subprocess
+import sublime
+import sublime_plugin
+import os
+import webbrowser
+import subprocess
 
 bCompareLocation = None
 isWindows = False
 
-#If we are on mac/unix
+# If we are on mac/unix
 if os.name == 'posix':
     bCompareLocation = '/usr/local/bin/bcompare'
 
-#Or we are on windows
+# Or we are on windows
 else:
     isWindows = True
     if os.path.exists("%s\Beyond Compare 4\BCompare.exe" % os.environ['ProgramFiles(x86)']):
         bCompareLocation = '"%s\Beyond Compare 4\BCompare.exe"' % os.environ['ProgramFiles(x86)']
     else:
-        bCompareLocation = '"%s\Beyond Compare 4\BCompare.exe"' % os.environ['ProgramFiles']
+        bCompareLocation = "%s\Beyond Compare 4\BCompare.exe" % os.environ['ProgramFiles']
 
 fileA = fileB = None
+
 
 def recordActiveFile(f):
     global fileA
@@ -23,21 +28,27 @@ def recordActiveFile(f):
     fileB = fileA
     fileA = f
 
+
 def runMacBeyondCompare():
-    if fileA != None and fileB != None:
-        cmd_line = str(bCompareLocation) + ' "' + str(fileA) + '" "' + str(fileB) + '"'
-        print("BeyondCompare comparing: LEFT [" + fileA + "] | RIGHT [" + fileB + "]")
+    if fileA is not None and fileB is not None:
+        print(
+            "BeyondCompare comparing: LEFT [" + fileA + "] | RIGHT [" + fileB + "]")
         subprocess.Popen([str(bCompareLocation), str(fileA), str(fileB)])
         print("Should be open...")
     else:
-        print("You must have activated TWO files to compare.\nPlease select two tabs to compare and try again")
-        sublime.error_message("You must have activated TWO files to compare.\nPlease select two tabs to compare and try again")
+        print(
+            "You must have activated TWO files to compare.\nPlease select two tabs to compare and try again")
+        sublime.error_message(
+            "You must have activated TWO files to compare.\nPlease select two tabs to compare and try again")
+
 
 def runWinBeyondCompare():
-    if fileA != None and fileB != None:
+    if fileA is not None and fileB is not None:
         cmd_line = '%s "%s" "%s"' % (bCompareLocation, fileA, fileB)
-        print("BeyondCompare comparing: LEFT [" + fileA + "] | RIGHT [" + fileB + "]")
+        print(
+            "BeyondCompare comparing: LEFT [" + fileA + "] | RIGHT [" + fileB + "]")
         subprocess.Popen(cmd_line)
+
 
 class BeyondCompareCommand(sublime_plugin.ApplicationCommand):
     def run(self):
@@ -53,9 +64,9 @@ class BeyondCompareCommand(sublime_plugin.ApplicationCommand):
         else:
             commandLinePrompt = sublime.ok_cancel_dialog('Could not find bcompare.\nPlease install the command line tools.', 'Do it now!')
             if commandLinePrompt:
-                new = 2 # open in a new tab, if possible
+                new = 2  # open in a new tab, if possible
                 url = "http://www.scootersoftware.com/support.php?zz=kb_OSXInstallCLT"
-                webbrowser.open(url,new=new)
+                webbrowser.open(url, new=new)
                 bCompareInstalled = sublime.ok_cancel_dialog('Once you have installed the command line tools, click the ok button to continue')
                 if bCompareInstalled:
                     if os.path.exists("/usr/local/bin/bcompare"):
@@ -66,7 +77,6 @@ class BeyondCompareCommand(sublime_plugin.ApplicationCommand):
 
                 else:
                     sublime.error_message('Please try again after you have command line tools installed.')
-
             else:
                 sublime.error_message('Please try again after you have command line tools installed.')
 
